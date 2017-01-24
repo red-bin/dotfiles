214a3793ebe93cb13c70c2568871fdbb82a2a305
Matt Chapman <hubblefisher@gmail.com>
2017-01-24 16:27:21 -0600

    Should have uploaded this a long time ago..

---
+++ profiles.clj
@@ -0,0 +1,13 @@
+{:user {:plugin-repositories [["private-plugins" {:url "private repo url"}]]
+        :dependencies [[pjstadig/humane-test-output "0.6.0"]]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
+        :plugins [[cider/cider-nrepl "0.8.2"]
+                  [refactor-nrepl "0.2.2"]
+                  [com.jakemccrary/lein-test-refresh "0.5.5"]
+                  [lein-autoexpect "1.4.2"]
+                  [lein-ancient "0.5.5"]
+                  [jonase/eastwood "0.2.1"]
+                  [lein-kibit "0.0.8"]
+                  [lein-pprint "1.1.2"]]
+        :test-refresh {:notify-command ["terminal-notifier" "-title" "Tests" "-message"]}}}
